0eb86e33515ba02cf7ef7fbcf5036f1778660ef4
Junichiro Kasuya <junichiro.kasuya@gmail.com>
2020-02-22 16:28:47 +0900

    add download binary and build image scripts

---
+++ build-images.sh
@@ -0,0 +1,5 @@
+pushd docker-images/OracleDatabase/SingleInstance/dockerfiles
+#  ./buildDockerImage.sh -v 18.3.0 -e
+  ./buildDockerImage.sh -v 18.4.0 -x
+#  ./buildDockerImage.sh -v 19.3.0 -e
+popd
---
+++ download-installer.sh
@@ -0,0 +1,1 @@
+aws s3 cp s3://oracle-docker-images/INSTALLER/LINUX.X64_193000_db_home.zip docker-images/OracleDatabase/SingleInstance/dockerfiles/19.3.0/
